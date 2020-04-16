823bec1ec617f47f52436b4a23bd58f31a972411
Benatti1991 <simone.benatti@studenti.unipr.it>
2020-04-16 09:34:06 +0200

    Fixed thingy in reward calc

--- Utils.py
+++ Utils.py
@@ -12,13 +12,23 @@ def plot(frame_idx, rewards):
 def CalcMeanRew(rew, not_dones):
     rewarr = np.asarray([line.cpu().numpy() for line in rew])
     not_donearr = np.asarray([line.cpu().numpy() for line in not_dones])
+    # Get an array which is = 1 when en episode is done
     donearr = np.ones(not_donearr.shape) - not_donearr
     rewsum = 0
+    undone_ep = 0
+    # Iterating over parallel episodes
     for i in range(donearr.shape[1]):
         done_ind = np.nonzero(donearr[:,i,:])
-        last_done = np.amax(done_ind[0])
+        # If an environment times out before being done even once, it is counted as one episode
+        if done_ind[0].size == 0:
+            last_done = -1
+            undone_ep += 1
+        # If not, we consider the rewards until the
+        else:
+            last_done = np.amax(done_ind[0])
         rewsum += np.sum( rewarr[0:last_done,i,:] )
-    mean_reward = rewsum / np.count_nonzero(donearr)
+    # Divide the total reward by the number of episodes
+    mean_reward = rewsum / (np.count_nonzero(donearr) + undone_ep )
     return mean_reward
 
 def cat_tuple_ob(states, tuple_len):
